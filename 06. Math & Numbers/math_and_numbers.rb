7bdffee56f68b0f7d23f3d7206e14a43e79909c4
Chathura Hettiarachchi <chathura93@yahoo.com>
2018-10-12 12:02:18 +0530

    Math try

---
+++ 06. Math & Numbers/math_and_numbers.rb
@@ -0,0 +1,16 @@
+num = 20
+puts('My number is ' + num.to_s)
+
+num = -10
+puts('My number is ' + num.abs.to_s)
+
+num = 1.22
+puts('My number is ' + num.round.to_s)
+
+num = 1.22
+puts('My number is ' + num.floor.to_s)
+
+num = 1.56
+puts('My number is ' + num.ceil.to_s)
+
+puts Math.sqrt(36)
